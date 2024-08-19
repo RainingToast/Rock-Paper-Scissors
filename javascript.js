13afff898f58e23cc721079564334e6b095087c4
RainingToast <162575588+RainingToast@users.noreply.github.com>
2024-08-19 17:47:25 +0800

    fix func getComputerChoice

--- javascript.js
+++ javascript.js
@@ -1,45 +1,13 @@
 function getComputerChoice() {
-    const randomNum = Math.floor(Math.random() * 3) + 1;
-
-    if (randomNum === 1) {
-        console.log("rock")
-        } else if (randomNum === 2) {
-        console.log("paper") }
-        else {
-        console.log ("scissors")
-    }
-}
-
-function getHumanChoice() {
-    let choice = prompt("Rock, Paper or Scissors?", "" )
-    return console.log(choice);
-}
-
-let humanScore = 0 ;
-let computerScore = 0 ; 
-
-function playRound(humanChoice, computerChoice) {
-    let result = "";
-   if (humanChoice == computerChoice) {
-    result = "it's a draw";
-   } else {
-    switch (humanChoice) {
-        case "rock":
-            result =  (computerChoice == "scissors") ? "You Win" : "You Lose";
-           break;
-        case "paper":
-            result =     (computerChoice == "rock") ? "You Win" : "You Lose";
+    let compChoice;
+    let randomNum = Math.floor(Math.random() * 3);
+        switch (randomNum) {
+            case 0: compChoice = "Rock";
             break;
-        case "scissors":
-            result =    (computerChoice == "paper") ? "You Win" : "You Lose";
-            break;       
+            case 1: compChoice =  "Paper";
+            break;
+            case 2: compChoice = "Scissors";
+            break;
     }
-    
-console.log(result);
-   }
+    console.log(compChoice);
 }
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-
-playRound(humanChoice, computerChoice);
-
