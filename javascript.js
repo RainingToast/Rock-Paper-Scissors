24f0a328295229595044f2df3fde6f3ef0f8c4e5
RainingToast <162575588+RainingToast@users.noreply.github.com>
2024-08-19 17:17:53 +0800

    restart whole thing with new getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-
-
 function getComputerChoice() {
     const randomNum = Math.floor(Math.random() * 3) + 1;
 
@@ -12,15 +10,36 @@ function getComputerChoice() {
     }
 }
 
-    
-
-getComputerChoice();
-
 function getHumanChoice() {
     let choice = prompt("Rock, Paper or Scissors?", "" )
     return console.log(choice);
 }
-getHumanChoice()
 
 let humanScore = 0 ;
-let computerScore = 0 ;
+let computerScore = 0 ; 
+
+function playRound(humanChoice, computerChoice) {
+    let result = "";
+   if (humanChoice == computerChoice) {
+    result = "it's a draw";
+   } else {
+    switch (humanChoice) {
+        case "rock":
+            result =  (computerChoice == "scissors") ? "You Win" : "You Lose";
+           break;
+        case "paper":
+            result =     (computerChoice == "rock") ? "You Win" : "You Lose";
+            break;
+        case "scissors":
+            result =    (computerChoice == "paper") ? "You Win" : "You Lose";
+            break;       
+    }
+    
+console.log(result);
+   }
+}
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
+
