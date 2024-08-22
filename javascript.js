acc379e4cacb1f5d1262290399e506731cbd7f00
RainingToast <162575588+RainingToast@users.noreply.github.com>
2024-08-22 20:29:18 +0800

    fix func playRound, create func scoreIncrease & make func playGame to hold both functions

--- javascript.js
+++ javascript.js
@@ -9,10 +9,57 @@ function getComputerChoice() {
             case 2: compChoice = "Scissors";
             break;
     }
-    return compChoice;
+    return compChoice.toLowerCase();
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt("Rock, Paper or Scissors?", );
-    return humanChoice;
+    let humanChoice = prompt("Rock, Paper or Scissors?");
+    return humanChoice.toLowerCase();
 }
+
+let humanScore =  0;
+let computerScore =  0;
+let result;
+
+
+function playRound () {
+   const humanChoice = getHumanChoice();
+   const computerChoice = getComputerChoice()
+
+    if (humanChoice === computerChoice) {
+        result === "Tie";
+    } else {
+        switch (humanChoice) {
+            case "rock" :
+               result = (computerChoice === "scissors") ? "You win": "You lose";
+               break;
+               case "paper" :
+                result = (computerChoice === "rock") ? "You win": "You lose";
+                break;
+                case "scissors" :
+                result = (computerChoice === "paper") ? "You win": "You lose";
+                break;   
+        }
+
+        }
+      console.log(result);
+        
+            
+}
+
+function scoreIncrease() {
+    if (result === "You win") {
+        humanScore +=1; console.log(humanScore, "Human Score,", computerScore, "Computer Score");
+    } else if (result === "Tie") {
+        console.log(humanScore, "Human Score," , computerScore, "Computer Score");
+    } else {
+        computerScore +=1; console.log(humanScore, "Human Score,", computerScore, "Computer Score");
+    }
+        
+}
+
+function playGame() {
+    playRound(), scoreIncrease();
+}
+
+playGame()
